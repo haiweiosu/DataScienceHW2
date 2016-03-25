2955a9f66627d127ea1f80bb5fc781a86990bd62
Sean Herman <seanherman@gmail.com>
2016-03-25 10:34:25 -0400

    MovieLens split

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 import findspark
 
 from pyspark import SparkContext, SparkConf
@@ -7,3 +8,5 @@ findspark.init()
 SPARK_CONF = SparkConf().setAppName("dshw2")
 SPARK_CONTEXT = SparkContext(conf=SPARK_CONF)
 SPARK_SQL_CONTEXT = SQLContext(SPARK_CONTEXT)
+
+DATA_PATH = os.path.join(os.path.realpath(__file__), "data")
